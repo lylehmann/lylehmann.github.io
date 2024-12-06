214a453de3666e6ded4e2acc74f7c766f444da7f
Lukas Yair Lehmann <llehmann84@outlook.com>
2024-12-06 10:55:29 +0100

    finalized first case study template

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -61,7 +61,7 @@ export const MoonIcon = () => (
 );
 
 export const EmailIcon = () => (
-	<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='size-6' fill="currentColor">
+	<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' className='size-6'>
 		<path
 			strokeLinecap='round'
 			strokeLinejoin='round'
