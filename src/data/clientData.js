c46ea321ca0ff4869d417370efb476fbcb0a0cf0
Lukas Yair Lehmann <llehmann84@outlook.com>
2024-12-06 12:49:52 +0100

    added about the client component

---
+++ src/data/clientData.js
@@ -0,0 +1,5 @@
+export const clientData = {
+  clientName: 'Focus Buddy',
+  clientDescription: 'Focus Buddy hilft Ihnen, Ihre Produktivität zu steigern.',
+  clientImage: '/images/focus-buddy.jpg'
+};
