c8facc5331b9c43122accaa6e1827a2f960d23bb
Lukas Yair Lehmann <llehmann84@outlook.com>
2024-12-09 12:39:48 +0100

    edited page meta data

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'LYLEHMANN';
+export const SITE_TAGLINE = 'Inclusive Digital Experiences | Lukas Yair Lehmann Digital Product Designer'; // site tagline
+export const SITE_DESCRIPTION =
+	'Lukas Yair Lehmann, a Digital Product Designer dedicated to making technology accessible for all. With a background in disability care and UX/UI design, I create inclusive, user-centered digital solutions'; // site meta description for SEO
